c3065f8e0abe9ac3bb22141cdb5db1a319f31390
gang.chen1 <gang.chen1@yintech.cn>
2022-11-10 18:48:50 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createSSRApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createRouter } from './router'
+
+export function createApp() {
+  const app = createSSRApp(App)
+  const router = createRouter()
+  app.use(router)
+  return { app, router }
+} 
