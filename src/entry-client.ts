76fe34080675c37e3bcacb6b4f67aebac1ccc03b
gang.chen1 <gang.chen1@yintech.cn>
2022-11-16 10:43:28 +0800

    feat: router

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,11 +1,8 @@
 import { createApp } from './main'
 
-const { app } = createApp()
+const { app, router } = createApp()
 
 // wait until router is ready before mounting to ensure hydration match
-// router.isReady().then(() => {
-//   app.mount('#app')
-// })
-setTimeout(() => {
+router.isReady().then(() => {
   app.mount('#app')
 })
